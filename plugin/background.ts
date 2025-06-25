5a78ade1e9062ba4342fb819ad185766ce76ceb1
Addon Bone <jacobneemila@gmail.com>
2025-06-25 17:43:57 +0300

    Update injecting logic and dependencies
    Refactor content script injection for improved error handling. Add new dependencies: @adnbn/inject-css and @adnbn/inject-script. Bump package version to 0.1.1. Update `package-lock.json` with updated module resolutions.

--- plugin/background.ts
+++ plugin/background.ts
@@ -15,42 +15,43 @@ export default defineBackground({
         if (!contents?.length) return;
 
         const executeContentScript = async (content: ContentScript) => {
-            const {
-                world,
-                matches,
-                run_at: runAt,
-                all_frames: frameId,
-                match_about_blank: matchAboutBlank,
-            } = content;
+            const {world, matches, run_at: runAt, all_frames: frameId, match_about_blank: matchAboutBlank} = content;
 
             const tabs = await queryTabs({url: matches});
 
             const scriptPromises = tabs
                 .filter(tab => tab.id !== undefined)
                 .map(async tab => {
-
                     const injectScript = injectScriptFactory({
                         tabId: tab.id!,
                         frameId,
                         matchAboutBlank,
-                        world
+                        world,
                     });
 
                     const injectCss = injectCssFactory({
                         tabId: tab.id!,
                         frameId,
                         matchAboutBlank,
-                        runAt
+                        runAt,
                     });
 
                     const promises = [];
 
                     if (content.js?.length) {
-                        promises.push(injectScript.file(content.js).catch(err => console.error(`ExecuteScript error on tab "${tab.title}":`, err)));
+                        promises.push(
+                            injectScript
+                                .file(content.js)
+                                .catch(err => console.error(`ExecuteScript error on tab "${tab.title}":`, err))
+                        );
                     }
 
                     if (content.css?.length) {
-                        promises.push(injectCss.file(content.css).catch(err => console.error(`InsertCSS error on tab "${tab.title}":`, err)));
+                        promises.push(
+                            injectCss
+                                .file(content.css)
+                                .catch(err => console.error(`InsertCSS error on tab "${tab.title}":`, err))
+                        );
                     }
 
                     return Promise.allSettled(promises);
